fa3169ec55d7dd815b1a956a4a9b5a28c4df067c
RollMan <qwpmb554@gmail.com>
2017-02-15 16:16:54 +0900

    Add rollman wrong solution of rollman 1

---
+++ rollman1/rollman_WA/rollman_WA.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+	int N; cin >> N;
+	double ans = 0.0;
+	for(int i = 0; i < N; i++){
+		int arg; cin >> arg;
+		ans += arg;
+	}
+	cout << ans / N << endl;
+	return 0;
+}
