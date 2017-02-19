ea1d8461a16a1797a9bad58b5e20bb9b89617796
Takuma Kageyama <uku09unk@gmail.com>
2017-02-20 00:21:11 +0900

    add solution

---
+++ uku1/uku_1/uku_binsearch.cpp
@@ -0,0 +1,111 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define all(v) begin(v), end(v)
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define reps(i, s, n) for(int i = (int)(s); i < (int)(n); i++)
+#define min(...) min({__VA_ARGS__})
+#define max(...) max({__VA_ARGS__})
+
+const int inf = 1LL << 55;
+const int mod = 1e9 + 7;
+
+struct UnionFind {
+  vector<int> data;
+  UnionFind(){}
+  UnionFind(int n):data(n, -1){}
+  int size(int x) {
+    return -data[find(x)];
+  }
+  int find(int x) {
+    return data[x] < 0 ? x : data[x] = find(data[x]);
+  }
+  bool same(int x, int y) {
+    return find(x) == find(y);
+  }
+  bool unite(int x, int y) {
+    x = find(x), y = find(y);
+    if(x == y) return false;
+    if(data[x] < data[y]) swap(x, y);
+    data[x] += data[y]; data[y] = x;
+    return true;
+  }
+};
+
+struct edge {
+  int u, v;
+  double w, t;
+  edge(){}
+  edge(int u, int v, double w, double t):u(u), v(v), w(w), t(t){}
+};
+
+#define MAX_M 10010
+
+int n, m;
+vector<edge> roads;
+
+UnionFind uf;
+bool used[MAX_M];
+double T, W;
+double Kruskal(double t)
+{
+  pair<double, int> key[MAX_M];
+  for(int i = 0; i < m; i++) {
+    key[i].first = roads[i].t - t*roads[i].w;
+    key[i].second = i;
+  }
+  sort(key, key + m, greater< pair<double, int> >());
+
+  uf = UnionFind(n);
+  fill(used, used + m, false);
+
+  for(auto& p : key) {
+    edge& e = roads[p.second];
+    if(uf.unite(e.u, e.v)) used[p.second] = true;
+  }
+
+  double sum = 0;
+  for(auto& p : key)
+    if(!used[p.second]) sum += p.first;
+
+  return sum <= 0.0;
+}
+
+double BinarySearch()
+{
+  double lb = 0, ub = 10000000000000;
+  for(int i = 0; i < 100; i++) {
+    double mb = (lb + ub) / 2;
+    if(Kruskal(mb)) ub = mb;
+    else lb = mb;
+  }
+
+  for(int i = 0; i < m; i++)
+    if(!used[i]) cout << i << endl;
+
+  //cout << T << " " << W << endl;
+  //return (W ? T/W : 0);
+  cout << lb << endl;
+  return ub;
+}
+
+signed main()
+{
+  cin.tie(0);
+  ios_base::sync_with_stdio(0);
+  cout << fixed << setprecision(12);
+
+  cin >> n >> m;
+  for(int i = 0; i < m; i++) {
+    int a, b;
+    double w, t;
+    cin >> a >> b >> w >> t;
+    roads.emplace_back(a, b, w, t);
+  }
+
+  cout << BinarySearch() << endl;
+
+  return 0;
+}
---
+++ uku1/uku_2/uku_dinkelbach.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define all(v) begin(v), end(v)
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define reps(i, s, n) for(int i = (int)(s); i < (int)(n); i++)
+#define min(...) min({__VA_ARGS__})
+#define max(...) max({__VA_ARGS__})
+
+const int inf = 1LL << 55;
+const int mod = 1e9 + 7;
+
+struct UnionFind {
+  vector<int> data;
+  UnionFind(){}
+  UnionFind(int n):data(n, -1){}
+  int size(int x) {
+    return -data[find(x)];
+  }
+  int find(int x) {
+    return data[x] < 0 ? x : data[x] = find(data[x]);
+  }
+  bool same(int x, int y) {
+    return find(x) == find(y);
+  }
+  bool unite(int x, int y) {
+    x = find(x), y = find(y);
+    if(x == y) return false;
+    if(data[x] < data[y]) swap(x, y);
+    data[x] += data[y]; data[y] = x;
+    return true;
+  }
+};
+
+struct edge {
+  int u, v;
+  double w, t;
+  edge(){}
+  edge(int u, int v, double w, double t):u(u), v(v), w(w), t(t){}
+};
+
+#define MAX_M 10010
+
+int n, m;
+vector<edge> roads;
+
+UnionFind uf;
+bool used[MAX_M];
+double fcost, gcost;
+double T, W;
+double Kruskal(double t)
+{
+  pair<double, int> key[MAX_M];
+  for(int i = 0; i < m; i++) {
+    key[i].first = roads[i].t - t*roads[i].w;
+    key[i].second = i;
+  }
+  sort(key, key + m, greater< pair<double, int> >());
+
+  uf = UnionFind(n);
+  fill(used, used + m, false);
+
+  fcost = gcost = 0;
+  for(auto& p : key) {
+    edge& e = roads[p.second];
+    if(uf.unite(e.u, e.v)) used[p.second] = true;
+  }
+
+  T = 0, W = 0;
+  for(int i = 0; i < m; i++)
+    if(!used[i]) T += roads[i].t, W += roads[i].w;
+  fcost = T, gcost = W;
+
+  return fcost - t*gcost;
+}
+
+double Dinkelbach()
+{
+  double t = 0;
+  for(t = 0; ; t = fcost / gcost)
+    if(fabs(Kruskal(t)) < 1e-8) break;
+
+  for(int i = 0; i < m; i++)
+    if(!used[i]) cout << i << endl;
+
+  return (W ? T/W : 0);
+}
+
+signed main()
+{
+  cin.tie(0);
+  ios_base::sync_with_stdio(0);
+  cout << fixed << setprecision(12);
+
+  cin >> n >> m;
+  for(int i = 0; i < m; i++) {
+    int a, b;
+    double w, t;
+    cin >> a >> b >> w >> t;
+    roads.emplace_back(a, b, w, t);
+  }
+
+  cout << Dinkelbach() << endl;
+
+  return 0;
+}
