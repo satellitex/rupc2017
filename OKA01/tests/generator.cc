bb15893ea9bb8f02373e3861c93b9f49d92f6c94
kawabys <s.kawabayashi@gmail.com>
2017-02-24 02:45:09 +0900

    a

--- OKA01/tests/generator.cc
+++ OKA01/tests/generator.cc
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(int argc, char *argv[]){
   registerGen(argc,argv,1);
-  for (int t=0;t<20;t++) {
+  for (int t=0;t<50;t++) {
     ofstream of(format("03_random_%02d.in",t+1).c_str());
     int n,p;
     n=rnd.next(1, 100);
