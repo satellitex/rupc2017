afbd9dae03845ae87ea766ced574235209f73994
Takuma Kageyama <uku09unk@gmail.com>
2017-02-25 13:41:41 +0900

    update

--- uku1/tests/generator.cpp
+++ uku1/tests/generator.cpp
@@ -3,15 +3,18 @@
 
 using namespace std;
 
-const int N = 10000;
-const int M = 10000;
+const int N = 100;
+const int M = 100;
 const int C = 1000000;
 const int T = 1000000;
 
 vector<int> X, Y;
 
 void init() {
-  mt19937 MT((int));
+  mt19937 MT((int)time(0));
+  Y.clear();
+  for(int i = 0; i <= N; i++) Y.push_back(i);
+  shuffle(Y.begin()+2, Y.end()-1, MT);
 }
 
 int main(int argc, char* argv[]) {
