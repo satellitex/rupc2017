e28f35d32c49a03873e68dbb1041edb8917c1613
kzyKT <s1200008aizu@gmail.com>
2017-03-01 00:07:03 +0900

    change solution

--- kzyKT2/kzyKT/kzyKT.cc
+++ kzyKT2/kzyKT/kzyKT.cc
@@ -6,14 +6,16 @@ typedef pair<int,int> P;
 #define N 100001
 vector<int> G[N],rG[N],g;
 vector<P> v[N];
-int n,cmp[N],u[N],c[N],a[N],d[N][2],z[N][2];
+int n,cmp[N],u[N],c[N],a[N],d[N][2],z[N][2],o[N],h[N],uu[N][2];
   
 void add_edge(int x,int y){G[x].push_back(y);rG[y].push_back(x);}
+
 void dfs(int x){
   u[x]=1;
   for(int i=0;i<G[x].size();i++)if(!u[G[x][i]])dfs(G[x][i]);
   g.push_back(x);
 }
+
 void rdfs(int x,int k){
   u[x]=1,cmp[x]=k;
   for(int i=0;i<rG[x].size();i++)if(!u[rG[x][i]])rdfs(rG[x][i],k);
@@ -28,24 +30,21 @@ int scc(){
   return k;
 }
 
-int p[N],r[N];
-void init(){for(int i=0;i<N;i++)p[i]=i,r[i]=0;}
-int find(int x){return (p[x]==x)?x:(p[x]=find(p[x]));}
+int q[N],r[N];
+void init(){for(int i=0;i<N;i++)q[i]=i,r[i]=0;}
+int find(int x){return (q[x]==x)?x:(q[x]=find(q[x]));}
 void unite(int x,int y) {
   x=find(x),y=find(y);
   if(x==y)return;
-  if(r[x]<r[y])p[x]=y;
-  else{p[y]=x;if(r[x]==r[y])r[x]++;}
+  if(r[x]<r[y])q[x]=y;
+  else{q[y]=x;if(r[x]==r[y])r[x]++;}
 }
-bool same(int x,int y){return find(x)==find(y);}
 
-int o[N],h[N];
-bool uu[N][2];
-void dfs(int x,int t,int q) {
+void dfs(int x,int t,int p) {
   for(int i=0;i<G[x].size();i++) {
     int y=G[x][i];
     if(uu[y][t^1]) continue;
-    if(q>=0&&cmp[y]==cmp[q]) unite(y,q);
+    if(p>=0&&cmp[y]==cmp[p]) unite(y,p);
     uu[y][t^1]=1;
     dfs(y,t^1,x);
   }
@@ -88,11 +87,15 @@ void calc() {
 }
 
 void make(){
-  for(int i=0;i<n;i++)for(int j=0;j<G[i].size();j++)if(cmp[i]!=cmp[G[i][j]])v[cmp[i]].push_back(P(G[i][j],o[find(i)]));
+  for(int i=0;i<n;i++){
+    for(int j=0;j<G[i].size();j++){
+      if(cmp[i]!=cmp[G[i][j]]) v[cmp[i]].push_back(P(G[i][j],o[find(i)]));
+    }
+  }
   for(int i=0;i<n;i++){
     sort(v[i].begin(),v[i].end());
     v[i].erase(unique(v[i].begin(),v[i].end()),v[i].end());
-    for(int j=0;j<v[i].size();j++)c[cmp[v[i][j].F]]++;
+    for(int j=0;j<v[i].size();j++) c[cmp[v[i][j].F]]++;
   }
 }
 
@@ -141,9 +144,7 @@ int main() {
     }
   }
   int ans=0;
-  for(int i=0; i<n; i++) {
-    for(int j=0; j<2; j++) ans=max(ans,d[cmp[i]][j]);
-  }
+  for(int i=0;i<n;i++)for(int j=0;j<2;j++) ans=max(ans,d[cmp[i]][j]);
   cout << ans << endl;
   return 0;
 }
