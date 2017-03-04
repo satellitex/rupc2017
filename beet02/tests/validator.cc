42fe0034c2b49e5f363ec09c3a2b4d5a8d518266
beet-aizu <aki.bdash@gmail.com>
2017-03-04 12:02:52 +0900

    add validation

--- beet02/tests/validator.cc
+++ beet02/tests/validator.cc
@@ -15,6 +15,15 @@ void input() {
 
 void check() {
   ensuref(s.size()<=100,"format invalid");
+  ensuref(s[0]=='x',"format invalid");
+  ensuref(s[1]=='^',"format invalid");
+  ensuref('1'<s[2]&&s[2]<='5',"n invalid");
+  for(int i=0;i<(int)s.size();i++){
+    if(!isdigit(s[i])) continue;
+    int tmp=0;
+    while(i<(int)s.size()&&isdigit(s[i])) tmp=tmp*10+s[i++]-'0';
+    ensuref(tmp<=2000,"coefficient invalid");
+  }
 }
 
 int main() {
