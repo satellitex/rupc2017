7f0f3340ad0194e360ceb6ebd8fc8f8c1167c046
RollMan <qwpmb554@gmail.com>
2017-03-08 18:56:58 +0900

    fix generator

--- rollman1/tests/generator.cc
+++ rollman1/tests/generator.cc
@@ -1,7 +1,5 @@
 #include "testlib.h"
 
-using namespace std;
-
 const int NUMBER_OF_THETA = 2;
 const int NUM_OF_TESTCASES = 10;
 const int MAXTHETA = 359;
@@ -12,21 +10,21 @@ int main(int argc, char *argv[])
     registerGen(argc, argv, 1);
 
     for(int t = 0; t < NUM_OF_TESTCASES; t++){
-        ofstream of(format("50_random_%02d.in", t+1).c_str());
+        std::ofstream of(format("50_random_%02d.in", t+1).c_str());
         int theta[NUMBER_OF_THETA];
         while(1){
             for(int i = 0; i < NUMBER_OF_THETA; i++){
                 theta[i] = rnd.next(MINTHETA, MAXTHETA);
             }
 
-            if( abs(theta[0] - theta[1]) == 180 || theta[0] + theta[1] == 360 ){
+            if( std::abs(theta[0] - theta[1]) == 180 || theta[0] + theta[1] == 360 ){
             }else{
                 break;
             }
         }
 
         for(int i = 0; i < NUMBER_OF_THETA; i++){
-            of << theta[i] << endl;
+            of << theta[i] << std::endl;
         }
 
         of.close();
