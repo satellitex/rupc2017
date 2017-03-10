b94111642c5dbf8c82e1c5e6437ade6f641fbe89
LeopardGecko <programming.ezweb.ne.jp@gmail.com>
2017-03-10 15:54:22 +0900

    add

--- beet01/gacho/gacho.cpp
+++ beet01/gacho/gacho.cpp
@@ -1,4 +1,5 @@
 #include<bits/stdc++.h>
+#define EPS (1e-8)
 #define N 100005
 #define M 4005
 using namespace std;
@@ -10,11 +11,11 @@ int main(){
   cin>>n;
   for(int i=0;i<n;i++){
     cin>>a[i];
-    A[(int)(a[i]*x)+1]++;
+    A[(int)(a[i]*x+EPS)+1]++;
   }
   for(int i=1;i<M;i++)A[i]+=A[i-1];
   for(int i=0;i<n;i++){
-    int p=(int)(a[i]*x)+1;
+    int p=(int)(a[i]*x+EPS)+1;
     cout<<A[p-1]*3+(A[p]-A[p-1]-1)<<endl;
   }
   return 0;
