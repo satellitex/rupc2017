ad974ffeb2bee165750682c66d427d6598ae91fc
LeopardGecko <programming.ezweb.ne.jp@gmail.com>
2017-03-10 15:39:20 +0900

    add

---
+++ beet01/gacho/gacho.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#define N 100005
+#define M 4005
+using namespace std;
+const double x=1000.0;
+int n,A[M];
+double a[N];
+
+int main(){
+  cin>>n;
+  for(int i=0;i<n;i++){
+    cin>>a[i];
+    A[(int)(a[i]*x)+1]++;
+  }
+  for(int i=1;i<M;i++)A[i]+=A[i-1];
+  for(int i=0;i<n;i++){
+    int p=(int)(a[i]*x)+1;
+    cout<<A[p-1]*3+(A[p]-A[p-1]-1)<<endl;
+  }
+  return 0;
+}
