8024cc01243483c4e12331d0432db5c4be7afd2b
Takuma Kageyama <uku09unk@gmail.com>
2017-03-11 16:52:36 +0900

    add uku solution

---
+++ aoba1/uku/uku.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define all(v) begin(v), end(v)
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define reps(i, s, n) for(int i = (int)(s); i < (int)(n); i++)
+#define min(...) min({__VA_ARGS__})
+#define max(...) max({__VA_ARGS__})
+
+template<class T1, class T2> void chmin(T1 &a, T2 b){if(a>b)a=b;}
+template<class T1, class T2> void chmax(T1 &a, T2 b){if(a<b)a=b;}
+
+using pint = pair<int, int>;
+using tint = tuple<int, int, int>;
+using vint = vector<int>;
+
+const int inf = 1LL << 55;
+const int mod = 1e9 + 7;
+
+int N;
+vint p;
+
+signed main()
+{
+  cin.tie(0);
+  ios_base::sync_with_stdio(0);
+  cout << fixed << setprecision(12);
+
+  cin >> N;
+  p.resize(N);
+  rep(i, N) cin >> p[i];
+
+
+
+  return 0;
+}
