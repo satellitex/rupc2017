0eacc511039e0fe57f03456e8e56cbd685d4eb08
Takuma Kageyama <uku09unk@gmail.com>
2017-03-11 23:02:31 +0900

    add uku solution

--- aoba1/uku/uku.cpp
+++ aoba1/uku/uku.cpp
@@ -19,8 +19,17 @@ using vint = vector<int>;
 const int inf = 1LL << 55;
 const int mod = 1e9 + 7;
 
+#define MAX_N 100000
+
 int N;
-vint p;
+int p[MAX_N];
+list<int> lis[MAX_N];
+int ans[MAX_N];
+
+/*
+ * えぇ…。強いデータ構造ってこういうことなの…？
+ * 嘘っぽい。
+ */
 
 signed main()
 {
@@ -29,10 +38,29 @@ signed main()
   cout << fixed << setprecision(12);
 
   cin >> N;
-  p.resize(N);
-  rep(i, N) cin >> p[i];
+  rep(i, N) {
+    cin >> p[i]; p[i]--;
+    lis[p[i]].push_back(i);
+  }
 
+  set<int> cha;
+  rep(i, N) cha.insert(i);
 
+  set<int> que;
+  rep(i, N) if(lis[i].size()) que.insert(i);
+
+  while(cha.size()) {
+    int idx = *que.begin(); que.erase(idx);
+    int head = lis[idx].front(); lis[idx].pop_front();
+    ans[idx] = head; cha.erase(idx);
+    if(lis[idx].empty()) continue;
+    auto it = cha.upper_bound(idx);
+    if(it == cha.end()) it = cha.begin();
+    lis[*it].splice(lis[*it].end(), move(lis[idx]));
+    que.insert(*it);
+  }
+
+  rep(i, N) cout << ans[i]+1 << endl;
 
   return 0;
 }
