d7a0d1994d8b47dc945e8f4961e78d6386816a3a
s1230149 <s1230149@gmail.com>
2017-03-13 01:14:06 +0900

    add case

--- kzyKT2/haji/haji.cpp
+++ kzyKT2/haji/haji.cpp
@@ -2,7 +2,6 @@
 #define MAX_V 100010
 #define N MAX_V
 using namespace std;
-
 /* 強連結成分分解 O(|V|+|E|) */
 int V; //頂点数
 vector<int> G[MAX_V]; //グラフの隣接リストの表現
@@ -18,14 +17,14 @@ void add_edge(int from,int to){
 
 void dfs(int v){
   used[v]=true;
-  for(int i=0;i<(int)G[v].size();i++)if(!used[G[v][i]])dfs(G[v][i]);
+  for(int i=0;i<G[v].size();i++)if(!used[G[v][i]])dfs(G[v][i]);
   vs.push_back(v);
 }
 
 void rdfs(int v,int k){
   used[v]=true;
   cmp[v]=k;
-  for(int i=0;i<(int)rG[v].size();i++) if(!used[rG[v][i]])rdfs(rG[v][i],k);
+  for(int i=0;i<rG[v].size();i++) if(!used[rG[v][i]])rdfs(rG[v][i],k);
 }
 
 int scc(){
@@ -39,42 +38,106 @@ int scc(){
   return k;
 }
 
+//////////////////////////////////////////////////////////////////////
+
 int C[N],nC[2][N];
-vector<int> g[N];
-void mkC(int pos,int f,int col){
+set<int> S[2][N];
+int mkC(int pos,bool f,int col){
+  if(S[f][col].count(pos)) return 0;
+  if(S[!f][col].count(pos)) return 1;
+  S[f][col].insert(pos);
   nC[f][col]+=C[pos];
+
   for(int i=0;i<(int)G[pos].size();i++){
     int nx = G[pos][i];
-    if(cmp[nx] == col)mkC(nx,!f,col);
+    if(cmp[nx] == col&&mkC(nx,!f,col)) return 1;
   }
+  return 0;
 }
 
-int calc(int pos,int f){
-  static int mem[N][2],used[N][2]={};
-  if(used[pos][f]++) return mem[pos][f];
-  
-  int res=0;
-  for(int i=0;i<(int)g[pos].size();i++)
-    for(int j=0;j<(int)G[g[pos][i]].size();j++){
-      int nx = cmp[G[g[pos][i]][j]];
-      if(nx==pos)continue;
-      res = max(res, calc(nx,!f));
+
+typedef pair<int,int> P;
+typedef pair<int,P> PP;
+set<PP> g[N];
+unordered_map<int,int>rg[2][N];
+int flg[N];
+int topological(){
+
+  queue<PP> Q;  
+  int res=0,D[2][N]={},scol=cmp[0],sidx = S[1][scol].count(0);
+
+  Q.push(PP(scol,P(0,sidx)));
+  D[0][scol] = nC[sidx][scol];
+
+  while(!Q.empty()){
+    PP t = Q.front();Q.pop();
+    int col = t.first;
+    int f = t.second.first;
+    int idx = t.second.second;
+    res = max(res,D[f][col]);
+
+    if(flg[col]){
+      D[!f][col] = max(D[!f][col],D[f][col]);
+      Q.push(PP(col,P(!f,idx)));
+      flg[col]=0;
     }
 
-  return mem[pos][f] = res + nC[pos][f]*(f==0);
+    for(auto it=g[col].begin();it!=g[col].end();it++){
+      int ncol = it->first;
+      int from = it->second.first;
+      int to = it->second.second;
+      int nf = (f + (idx==from))%2;
+      int c = nf==0? nC[to][ncol]:nC[!to][ncol];
+      D[nf][ncol]=max(D[nf][ncol],D[f][col]+c);
+      rg[nf][ncol].erase(col);
+      if(rg[nf][ncol].empty())Q.push(PP(ncol,P(nf,to)));
+    }
+  }  
+
+  return res;
 }
 
 int main(){
+  return 0;
   int n,m;
   cin>>n>>m;
-  for(int i=0;i<n;i++) cin>>C[i];
-  for(int i=0,a,b;i<m;i++)cin>>a>>b,add_edge(a,b);
+  for(int i=0;i<n;i++)scanf("%d",&C[i]);
+  for(int i=0,a,b;i<m;i++)scanf("%d%d",&a,&b),add_edge(a,b);
   V = n;
-  n = scc();
+  V = scc();
+
   int used[N]={};
-  for(int i=0;i<n;i++)if(!used[cmp[i]]++) mkC(i,0,cmp[i]);
-  for(int i=0;i<n;i++) g[cmp[i]].push_back(i);
+  for(int i=0;i<n;i++){
+    int col = cmp[i];
+    if(!used[col]++&&mkC(i,0,col)){
+	flg[col] = 1;
+	nC[0][col] = nC[1][col]=0;
+      }
+    }
+  for(int i=0;i<n;i++){ 
+    int col = cmp[i];
+    if(flg[col]){
+      nC[0][col]+=C[i];
+      nC[1][col]+=C[i];
+      S[0][col].insert(i);
+      S[1][col].insert(i);
+    }
+  }
+
+  for(int i=0;i<n;i++)
+    for(int j=0;j<G[i].size();j++){
+      int col = cmp[i];
+      int nv = G[i][j];
+      int ncol = cmp[nv];
+      int from = S[1][col].count(i);
+      int to = S[1][ncol].count(nv);
+      if(col==ncol)continue;
+      g[col].insert(PP(ncol,P(from,to)));
+      rg[0][ncol][col]=1;
+      rg[1][ncol][col]=1;
+    }
   
-  cout<< calc(cmp[0],0)<<endl;
+
+  cout<<topological()<<endl;
   return 0;
 }
