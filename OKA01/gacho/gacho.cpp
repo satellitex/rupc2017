44ed8c1aaabd055f0c163c3c2234f8976b6dffdc
LeopardGecko <programming.ezweb.ne.jp@gmail.com>
2017-03-15 01:32:02 +0900

    a

--- OKA01/gacho/gacho.cpp
+++ OKA01/gacho/gacho.cpp
@@ -1,8 +1,9 @@
 #include<bits/stdc++.h>
+#define INF (1e9)
 #define P 1000005
 #define N 105
 using namespace std;
-int n,p,x[N],y[N],A[N][N],dp[2][N][P],ans;
+int n,p,x[N],y[N],A[N][N],dp[N][N][N],ans;
 
 int main(){
   cin>>n>>p;
@@ -11,19 +12,23 @@ int main(){
   for(int i=0;i<n;i++)
     for(int j=i;j<n;j++)
       A[i][j]=max(0,y[j]-max(0,(x[j]-(j?A[i][j-1]:0))));
-  /*  for(int i=0;i<n;i++)
-    for(int j=i;j<n;j++)
-    A[i][j]=min(y[j],max(0,y[j]-x[j]+(j?A[i][j-1]:0)));*/
-  for(int i=0;i<n;i++){
-    for(int j=i;j>=0;j--)
-      for(int k=A[i-j][i];k<=p;k++){
-	dp[(i+1)%2][j+1][k-A[i-j][i]]=max(dp[(i+1)%2][j+1][k-A[i-j][i]],dp[i%2][j][k]+1);
-	ans=max(ans,dp[(i+1)%2][j+1][k-A[i-j][i]]);
+  for(int i=0;i<=n;i++)
+    for(int j=0;j<=n;j++)
+      for(int k=0;k<=n;k++)
+	dp[i][j][k]=INF;
+  dp[0][0][0]=0;
+  for(int i=0;i<n;i++)
+    for(int j=0;j<=i;j++)
+      for(int k=0;k<n;k++){
+	if(dp[i][j][k]==INF)continue;
+	if(A[i-j][i]<=p-dp[i][j][k])
+	  dp[i+1][j+1][k+1]=min(dp[i+1][j+1][k+1],dp[i][j][k]+A[i-j][i]);
+	dp[i+1][0][k]=min(dp[i+1][0][k],dp[i][j][k]);
       }
-    for(int j=i;j>=0;j--)
-      for(int k=0;k<=p;k++)
-	dp[(i+1)%2][0][k]=max(dp[(i+1)%2][0][k],dp[i%2][j][k]);
-  }
+  for(int i=0;i<=n;i++)
+    for(int j=0;j<=n;j++)
+      for(int k=0;k<=n;k++)
+	if(dp[i][j][k]!=INF)ans=max(ans,k);
   cout<<ans<<endl;
   return 0;
 }
