d75bd4515aeafc436f2d2219ddbf62ecd5daaa9c
Takuma Kageyama <uku09unk@gmail.com>
2017-03-16 21:57:43 +0900

    add testcase

--- uku3/tests/gen_4ary.cc
+++ uku3/tests/gen_4ary.cc
@@ -3,36 +3,27 @@
 
 using namespace std;
 
-const int N = 10;
-const int Q = 10;
+const int N = 10000;
+const int Q = 10000;
 
 int main(int argc, char* argv[]) {
   registerGen(argc, argv, 1);
 
-  for(int t = 0; t < 10; t++) {
-    ofstream of(format("01_small_%02d.in", t+1).c_str());
+  for(int t = 0; t < 30; t++) {
+    ofstream of(format("06_4ary_%02d.in", t+1).c_str());
     int n = rnd.next(1, N);
-    int q = rnd.next(1, Q);
+    int q = n*10;//rnd.next(1, Q);
     of << n << " " << q << endl;
     for(int v = 1; v < n; v++) {
       int u = rnd.next(0, v-1);
       of << u << " " << v << endl;
     }
-    set<int> st;
-    st.insert(0);
     for(int i = 0; i < q; i++) {
       int t = rnd.next(1, 2);
       int r = rnd.next(0, n-1);
-      int x = rnd.next(0, 4);
-      int y = rnd.next(0, 4);
-      if(t == 1) {
-	while(!st.count(y)) y = rnd.next(0, 9);
-	if(x > y) swap(x, y);
-      } else {
-	while(!st.count(x)) x = rnd.next(0, 9);
-	st.erase(x);
-	st.insert(y);
-      }
+      int x = rnd.next(0, 3);
+      int y = rnd.next(0, 3);
+      if(t == 1 && x > y) swap(x, y);
       of << t << " " << r << " " << x << " " << y << endl;
     }
     of.close();
--- uku3/tests/generator.cc
+++ uku3/tests/generator.cc
@@ -9,7 +9,7 @@ const int Q = 100000;
 int main(int argc, char* argv[]) {
   registerGen(argc, argv, 1);
 
-  for(int t = 0; t < 10; t++) {
+  for(int t = 0; t < 20; t++) {
     ofstream of(format("03_random_%02d.in", t+1).c_str());
 
     int n = rnd.next(1, N);
@@ -32,12 +32,21 @@ int main(int argc, char* argv[]) {
       of << uu << " " << vv << endl;
     }
 
+    set<int> st;
+    st.insert(0);
     for(int i = 0; i < q; i++) {
       int t = rnd.next(1, 2);
       int r = rnd.next(0, n-1);
       int x = rnd.next(0, 9);
       int y = rnd.next(0, 9);
-      if(t == 1 && x > y) swap(x, y);
+      if(t == 1) {
+	while(!st.count(y)) y = rnd.next(0, 9);
+	if(x > y) swap(x, y);
+      } else {
+	while(!st.count(x)) x = rnd.next(0, 9);
+	st.erase(x);
+	st.insert(y);
+      }
       of << t << " " << r << " " << x << " " << y << endl;
     }
 
