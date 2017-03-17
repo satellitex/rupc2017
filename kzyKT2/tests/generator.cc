735303cda14838eeee0ebbf70c5235669952f994
kzyKT <s1200008aizu@gmail.com>
2017-03-17 21:24:04 +0900

    add testcase

--- kzyKT2/tests/generator.cc
+++ kzyKT2/tests/generator.cc
@@ -7,7 +7,7 @@ ll N=100000;
 int main(int argc, char* argv[]) {
   registerGen(argc, argv);
   for(int t=0; t<30; t++) {
-    ofstream ofs(format("03_random_%02d.in",t).c_str());
+    ofstream ofs(format("02_random_%02d.in",t).c_str());
     ll n=rnd.next(1LL,N),m=rnd.next(0LL,min(n*(n-1),N));
     ofs << n << " " << m << endl;
     for(int i=0; i<n; i++) {
