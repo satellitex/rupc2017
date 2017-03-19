466b7eb4257a51a26b1f2c33d2d390ce584564a0
Takuma Kageyama <uku09unk@gmail.com>
2017-03-19 20:04:57 +0900

    update test

--- uku3/tests/gen_hand.cc
+++ uku3/tests/gen_hand.cc
@@ -6,35 +6,39 @@ using namespace std;
 int main(int argc, char* argv[]) {
   registerGen(argc, argv, 1);
 
-  for(int t = 0; t < 1; t++) {
+  for(int t = 0; t < 2; t++) {
     ofstream of(format("10_hand_%02d.in", t+1).c_str());
     int n = 100000;
     int q = 100000;
     of << n << " " << q << endl;
-    queue< pair<int, int> > que;
-    que.push(make_pair(1, 0));
-    que.push(make_pair(2, 0));
-    int to = 3, ed = 0;
-    while(!que.empty()) {
-      int cur, par;
-      tie(cur, par) = que.front(); que.pop();
-      if(cur == n) break;
-      ed++;
-      of << par << " " << cur << endl;
-      que.push(make_pair(to++, cur));
-      que.push(make_pair(to++, cur));
+    if(t == 0) {
+      queue< pair<int, int> > que;
+      que.push(make_pair(1, 0));
+      que.push(make_pair(2, 0));
+      int to = 3, ed = 0;
+      while(!que.empty()) {
+	int cur, par;
+	tie(cur, par) = que.front(); que.pop();
+	if(cur == n) break;
+	ed++;
+	of << par << " " << cur << endl;
+	que.push(make_pair(to++, cur));
+	que.push(make_pair(to++, cur));
+      }
+    } else {
+      for(int v = 1; v < n; v++) {
+	of << v-1 << " " << v << endl;
+      }
     }
-    //assert(ed == n-1);
     while(q > 0) {
       for(int i = 0; i < 10 && q > 0; i++, q--) {
 	of << 2 << " " << 0 << " " << i << " " << (i+1)%10 << endl;
       }
       if(q > 0) {
-	of << 1 << " " << 0 << " " << 0 << " " << 9 << endl;
-	q--;
+	of << 1 << " " << 0 << " " << 0 << " " << 9 << endl; q--;
       }
     }
-    //assert(q == 0);
+
     of.close();
   }
 
