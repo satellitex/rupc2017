741b3380b9fe7c19aa67387edbfd64f28884cf49
s1230149 <s1230149@gmail.com>
2017-03-21 07:25:14 +0900

    fix SelectSets

--- SelectSet/haji/haji.cpp
+++ SelectSet/haji/haji.cpp
@@ -1,32 +1,23 @@
 #include <bits/stdc++.h>
-#define N 20001
+#define N 20000
 using namespace std;
 typedef unsigned long long ull;
-const ull B = 1777771;
-int n,num[N],cost[1<<22],flg[1<<22];
+int n,num[N],cost[1<<22];
+bool flg[1<<22];
 set<short> cnt[1<<22];
 void Max(int &a,int b){a=max(a,b);};
-int count(int a){return __builtin_popcount(a);}
-
-ull StoH(set<short> &S){
-  ull res=0;
-  for(auto it=S.begin();it!=S.end();it++)res=res*B+(*it+1);
-  return res;  
-}
+char count(int a){return __builtin_popcount(a);}
 
 void marge(set<short>&a,set<short>&b){
-  for(auto it=b.begin();it!=b.end()&&a.size()<3;it++) a.insert(*it);
+  for(auto it=b.begin();a.size()<3&&it!=b.end();it++) a.insert(*it);
 }
 
-ull H[1<<22];
 void mkCost(){
-
   for(int i=0;i<n;i++){
     int a = num[i];
     cost[a]|=a;
     if(cnt[a].size()<3)cnt[a].insert(i);
     flg[a]=cnt[a].size()>=3;
-    H[a] = StoH(cnt[a]);
   }
   
   for(int i=(1<<22)-1;i>=0;i--){
@@ -35,9 +26,9 @@ void mkCost(){
       int nbit = i ^ (1<<j);
       set<short> &I=cnt[i],&J=cnt[nbit];
       cost[nbit] |= cost[i];
-      if(flg[nbit]||H[i]==H[nbit])continue;
+      if(flg[nbit])continue;
       if(flg[i])flg[nbit]=1,J.clear();
-      else marge(J,I),H[nbit] = StoH(J);
+      else marge(J,I);
       if(J.size()>=3) flg[nbit]=1,J.clear();
     }
     cnt[i].clear();
