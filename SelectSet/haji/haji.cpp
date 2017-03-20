b896b7209a6b74577a01b2255d8ed71a7f8f03b1
s1230149 <s1230149@gmail.com>
2017-03-21 04:31:53 +0900

    fix HTML/H

--- SelectSet/haji/haji.cpp
+++ SelectSet/haji/haji.cpp
@@ -1,10 +1,10 @@
 #include <bits/stdc++.h>
-#define N 40001
+#define N 20001
 using namespace std;
-int n;
-int num[N];
+int n,num[N];
 int cost[1<<22];
-set<int> cnt[1<<22];
+set<short> cnt[1<<22];
+int flg[1<<22];
 
 void Max(int &a,int b){a=max(a,b);};
 int count(int a){return __builtin_popcount(a);}
@@ -23,6 +23,8 @@ void mkCost(){
       else 
 	for(auto it=cnt[i].begin();it!=cnt[i].end();it++)cnt[nbit].insert(*it);
     }
+    flg[i] = cnt[i].size();
+    cnt[i].clear();
   }
 }
 
@@ -38,7 +40,7 @@ int main(){
 
   int ans=0;
   for(int i=0;i<(1<<22);i++){
-    if(cnt[i].size()<3)continue;
+    if(flg[i]<3)continue;
     Max(ans,count(i)*count(cost[i]));
   }
   cout<<ans<<endl;
