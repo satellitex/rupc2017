4d8827a55e5ca5c363bb17f824d9faec3a7800e0
LeopardGecko <programming.ezweb.ne.jp@gmail.com>
2017-03-21 12:44:33 +0900

    a

--- SelectSet/gacho/gacho2.cpp
+++ SelectSet/gacho/gacho2.cpp
@@ -1,18 +1,10 @@
 #include<bits/stdc++.h>
 #define M 22
 using namespace std;
+set<short> s[(1<<M)];
 int n,k,Or[(1<<M)],nS;
-short s[(1<<M)][3];
-
-void merge(int S,int x){
-  for(int i=0;i<3;i++){
-    if(s[S][i]==x)return;
-    if(s[S][i]==-1){
-      s[S][i]=x;
-      return;
-    }
-  }
-}
+bool flag[(1<<M)];
+set<short>::iterator ite;
 
 int solve(){
   int res=0;
@@ -21,13 +13,19 @@ int solve(){
       if(!(S&(1<<i)))continue;
       nS=(S&~(1<<i));
       Or[nS]|=Or[S];
-      for(int j=0;j<3;j++)
-	if(s[S][j]!=-1)merge(nS,s[S][j]);
+      ite=s[S].begin();
+      while(ite!=s[S].end()&&s[nS].size()<3){
+	s[nS].insert((*ite));
+	ite++;
+      }
+      if(s[nS].size()==3){
+	flag[nS]=1;
+	s[nS].clear();
+      }
     }
-    if(s[S][2]!=-1)
-      res=max(res,
-	      __builtin_popcount(Or[S])
-	      *__builtin_popcount(S));
+    if(flag[S])
+      res=max(res,__builtin_popcount(Or[S])*__builtin_popcount(S));
+    s[S].clear();
   }
   return res;
 }
@@ -35,13 +33,16 @@ int solve(){
 int main(){
   cin>>n;
   int S,a;
-  memset(s,-1,sizeof(s));
   for(int i=0;i<n;i++){
     scanf("%d",&k);
     S=0;
     for(int j=0;j<k;j++)
       scanf("%d",&a),S|=(1<<(a-1));
-    if(s[S][2]==-1)merge(S,i);
+    if(!flag[S]&&s[S].size()<3)s[S].insert(i);
+    if(s[S].size()==3){
+      flag[S]=1;
+      s[S].clear();
+    }
     Or[S]|=S;
   }
   cout<<solve()<<endl;
